535ba74ebd6b9651a4fb992be7f7f6ef526c3002
James H <jamesherrero@gmail.com>
2019-10-04 14:45:04 +1000

    Add empty custom view, TodoView - Java
    We will begin creating a custom view that will abstract out the funcitonality found within our TaskAdapter
      - implements inflation of view
      - implements assignment of text / values
      - implement Striking text on checkbox click (line through text)
    
    TodoView.java
      - Empty Java Class (intention to be a custom view)
      - Extends ConstraintLayout
      - Applies 3 naked constructors

---
+++ app/src/main/java/intermediate/course/views/TodoView.java
@@ -0,0 +1,21 @@
+package intermediate.course.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class TodoView extends ConstraintLayout {
+
+    public TodoView(Context context) {
+        super(context);
+    }
+
+    public TodoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TodoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
