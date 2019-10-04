29cca5602c0860dcfae71189bdbb7f63b7fbcae2
James H <jamesherrero@gmail.com>
2019-10-04 14:25:44 +1000

    Refactor TaskAdapter.kt to shorthand
    TaskAdapter.kt
      Implement shorthand to our inflate viewS

--- app/src/main/java/intermediate/course/tasks/TaskAdapter.kt
+++ app/src/main/java/intermediate/course/tasks/TaskAdapter.kt
@@ -7,8 +7,10 @@ import androidx.recyclerview.widget.RecyclerView
 import intermediate.course.R
 import intermediate.course.foundations.BaseRecyclerAdapter
 import intermediate.course.models.Task
+import kotlinx.android.synthetic.main.item_note.view.*
 import kotlinx.android.synthetic.main.item_task.view.*
 import kotlinx.android.synthetic.main.view_todo.view.*
+import kotlinx.android.synthetic.main.view_todo.view.descriptionView
 
 // Convert construct from val to param by removing val
 class TaskAdapter(
@@ -26,19 +28,16 @@ class TaskAdapter(
             
             // setup custom view & assign values
             data.todos.forEach {todo ->
-                // inflate each todo and programatically attached it to a parent (item_task @todoContainer & attach view_todo)
-                val todoView = LayoutInflater.from(view.context).inflate(R.layout.view_todo, view.todoContainer, false)
 
-                // todoView holds our inflated view @view_todo
-                todoView.descriptionView.text = todo.description  // assign the value
-
-
-                // Add Check for checkbox
-                if (todo.isComplete) {
-                    todoView.completedCheckBox.isChecked = true
-                }
-
-                // todoView.completedCheckBox.isChecked = todo.isComplete  // alternative code line
+                // inflate view, & apply view
+                val todoView =
+                    LayoutInflater
+                        .from(view.context)
+                        .inflate(R.layout.view_todo, view.todoContainer, false)
+                        .apply {
+                            descriptionView.text = todo.description
+                            completedCheckBox.isChecked = todo.isComplete
+                        }
 
                 // Programmatically add view as a child of LinearLayout @todoContainer
                 view.todoContainer.addView(todoView)
